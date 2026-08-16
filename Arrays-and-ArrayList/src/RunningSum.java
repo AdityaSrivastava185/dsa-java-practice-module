1a3462ed70d0dcd5330eb9a8c91a042d9105a8eb
Aditya Srivastava <pks85715@gmail.com>
2026-08-17 00:34:32 +0530

    add the solution for the running sum of array

---
+++ Arrays-and-ArrayList/src/RunningSum.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class RunningSum {
+    static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(runningsum(nums)));
+    }
+    public static int[] runningsum(int[] nums){
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = nums[i] + ans[i - 1];
+        }
+        return ans;
+    }
+}
