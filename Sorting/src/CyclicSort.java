0128ab0e0d17bf315678f5e60bd9b76177e5cad9
Aditya Srivastava <pks85715@gmail.com>
2026-08-26 16:45:48 +0530

    add the algorithm for cyclic sort

---
+++ Sorting/src/CyclicSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    static void main(String[] args) {
+        int[] nums = {3, 5, 2, 1, 4, 7, 6, 8, 10, 9};
+        cyclicSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void cyclicSort(int[] nums){
+        int i = 0;
+        while(i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }else{
+                i++;
+            }
+        }
+    }
+}
