1fe0e7881112eab6628436d0721f7a5325bfd6fa
Aditya Srivastava <pks85715@gmail.com>
2026-09-13 15:36:27 +0530

    bitwise opertor in java file added

---
+++ Bitwise/src/Main.java
@@ -0,0 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main{
+    static void main(String[] args) {
+        int[][] num = {
+                {1, 1, 0},
+                {1, 0, 1},
+                {0, 0, 0}
+        };
+
+        int[][] ans = flipImage(num);
+
+        System.out.println(Arrays.deepToString(ans));
+    }
+    public static int[][] flipImage(int[][] image) {
+        for(int[] row : image) {
+            // reverse this array
+            for (int i = 0; i < (image[0].length + 1) / 2; i++) {
+                // swap
+                int temp = row[i] ^ 1;
+                row[i] = row[image[0].length - i - 1] ^ 1;
+                row[image[0].length - i - 1] = temp;
+            }
+        }
+        return image;
+    }
+}
